e167bb74b82f1f2cf6c084568e37012c0e127eac
Tyler Myracle <tyler.myracle@gmail.com>
2021-06-17 09:39:47 -0700

    fix password reset email link

--- server/app/mailers/reset_password_mailer.rb
+++ server/app/mailers/reset_password_mailer.rb
@@ -4,6 +4,7 @@ class ResetPasswordMailer < ApplicationMailer
   def send_password_reset_email(user)
     # TODO: Build out mailer with link and token
     @user = user
+    @url = "http://localhost:3000/reset_password/#{@user.reset_password_token}"
     mail(:to => @user.email, :subject => "Reset your password")
   end
 end
