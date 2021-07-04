3f47018f53a5fcbc7ec815800ee10f40499c8bde
Tyler Myracle <tyler.myracle@gmail.com>
2021-07-04 13:28:00 -0700

    finish vendors tests

--- server/spec/requests/vendors_spec.rb
+++ server/spec/requests/vendors_spec.rb
@@ -10,33 +10,60 @@ describe 'Vendors API', type: :request do
     @token = response_json["token"]
   end
 
-  it 'should not allow unauthorized request for vendor list' do
-    get '/api/v1/vendor_list'
-    
-    expect(response).to have_http_status(:unauthorized)
+  context 'when creating new vendors' do
+    it 'should not create if unauthorized' do
+      post '/api/v1/vendors', params: {name: "New Vendor", website: "www.newvendor.com", description: "newest vendor on the block"}
+      expect(response).to have_http_status(:unauthorized)
+    end
+  
+    it 'should create new vendor when authorized' do
+      post '/api/v1/vendors', params: {name: "New Vendor", website: "www.newvendor.com", description: "newest vendor on the block"}, headers: {"Authorization": "Bearer #{@token}"}
+      expect(response).to have_http_status(:success)
+    end
+  
+    it 'should return vendor after created' do
+      post '/api/v1/vendors', params: {name: "New Vendor", website: "www.newvendor.com", description: "newest vendor on the block"}, headers: {"Authorization": "Bearer #{@token}"}
+      expect(JSON.parse(response.body)).to have_key("id")
+    end
+  
+  
+    it 'should not create vendor with empty name' do
+      post '/api/v1/vendors', params: {name: "", website: "www.newvendor.com", description: "newest vendor on the block"}, headers: {"Authorization": "Bearer #{@token}"}
+      expect(response).to have_http_status(:internal_server_error)
+    end
   end
 
-  it 'returns the vendor list' do
-    v1 = FactoryBot.create(:vendor, name: "Alpha Co", description: "We are alpha!", website: "www.alpha.com", company_id: @company.id)
-    v2 = FactoryBot.create(:vendor, name: "Beta Co", description: "We are beta!", website: "www.beta.com", company_id: @company.id)
-    v3 = FactoryBot.create(:vendor, name: "Gamma Co", description: "We are gamma!", website: "www.gamma.com", company_id: @company.id)
-    get '/api/v1/vendor_list', headers: {"Authorization": "Bearer #{@token}"}
-    
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(3)
+  context 'when accessing vendors list' do
+    it 'should not allow unauthorized request for vendor list' do
+      get '/api/v1/vendor_list'
+      expect(response).to have_http_status(:unauthorized)
+    end
+  
+    it 'returns successful response from vendors list if authorized' do
+      get '/api/v1/vendor_list', headers: {"Authorization": "Bearer #{@token}"}
+      expect(response).to have_http_status(:success)
+    end
+  
+    it 'returns the correct number of vendors' do
+      v1 = FactoryBot.create(:vendor, name: "Alpha Co", description: "We are alpha!", website: "www.alpha.com", company_id: @company.id)
+      v2 = FactoryBot.create(:vendor, name: "Beta Co", description: "We are beta!", website: "www.beta.com", company_id: @company.id)
+      v3 = FactoryBot.create(:vendor, name: "Gamma Co", description: "We are gamma!", website: "www.gamma.com", company_id: @company.id)
+      get '/api/v1/vendor_list', headers: {"Authorization": "Bearer #{@token}"}
+      
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
+  
+    it 'should group vendor list by first letter' do
+      v1 = FactoryBot.create(:vendor, name: "Alpha Co", description: "We are alpha!", website: "www.alpha.com", company_id: @company.id)
+      v2 = FactoryBot.create(:vendor, name: "America Co", description: "We are america!", website: "www.america.com", company_id: @company.id)
+      v3 = FactoryBot.create(:vendor, name: "Gamma Co", description: "We are gamma!", website: "www.gamma.com", company_id: @company.id)
+      get '/api/v1/vendor_list', headers: {"Authorization": "Bearer #{@token}"}
+      
+      expect(response).to have_http_status(:success)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response.size).to eq(2)
+      expect(parsed_response).to have_key("A")
+      expect(parsed_response["A"].size).to eq(2)
+    end
   end
-
-  it 'should group vendor list by first letter' do
-    v1 = FactoryBot.create(:vendor, name: "Alpha Co", description: "We are alpha!", website: "www.alpha.com", company_id: @company.id)
-    v2 = FactoryBot.create(:vendor, name: "America Co", description: "We are america!", website: "www.america.com", company_id: @company.id)
-    v3 = FactoryBot.create(:vendor, name: "Gamma Co", description: "We are gamma!", website: "www.gamma.com", company_id: @company.id)
-    get '/api/v1/vendor_list', headers: {"Authorization": "Bearer #{@token}"}
-    
-    expect(response).to have_http_status(:success)
-    parsed_response = JSON.parse(response.body)
-    expect(parsed_response.size).to eq(2)
-    expect(parsed_response).to have_key("A")
-    expect(parsed_response["A"].size).to eq(2)
-  end
-
 end
