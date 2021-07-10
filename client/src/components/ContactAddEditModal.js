e3d5c0c8efb47e629ff327516fccbf3a4ef54cf1
Tyler Myracle <tyler.myracle@gmail.com>
2021-07-10 15:04:02 -0700

    refactor add contact to use react hook form

--- client/src/components/ContactAddEditModal.js
+++ client/src/components/ContactAddEditModal.js
@@ -1,66 +1,34 @@
 import React, { useState, Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import { withToken } from "../lib/authHandler";
 import { UserAddIcon } from "@heroicons/react/solid";
 
 const ContactAddEditModal = (props) => {
   const [errorMessage, setErrorMessage] = useState("");
-  const [name, setName] = useState("");
-  const [title, setTitle] = useState("");
-  const [primaryPhone, setPrimaryPhone] = useState("");
-  const [secondaryPhone, setSecondaryPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [notes, setNotes] = useState("");
+  const { register, handleSubmit, reset } = useForm();
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
-
-  const handlePrimaryPhoneChange = (e) => {
-    setPrimaryPhone(e.target.value);
-  };
-
-  const handleSecondaryPhoneChange = (e) => {
-    setSecondaryPhone(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleNotesChange = (e) => {
-    setNotes(e.target.value);
-  };
-
-  const handleContactAddSubmit = async () => {
+  const handleContactAddSubmit = async (data) => {
     const payload = {
-      name: name,
-      title: title,
-      primary_phone: primaryPhone,
-      secondary_phone: secondaryPhone,
-      email: email,
-      notes: notes,
+      name: data.name,
+      title: data.title,
+      primary_phone: data.primaryPhone,
+      secondary_phone: data.secondaryPhone,
+      email: data.email,
+      notes: data.notes,
       vendor_id: props.vendor.id,
     };
 
     try {
       const res = await axios.post("/api/v1/contacts", payload, withToken());
       if (res.status === 200) {
-        console.log(res.data);
         props.toggleContactAddEditModal();
+        reset();
       }
     } catch (err) {
       setErrorMessage(err.response.data.message);
     }
-  };
-
-  const handleContactEditSubmit = async () => {
-    console.log(`Editing: ${name}`);
   };
 
   return (
@@ -104,158 +72,159 @@ const ContactAddEditModal = (props) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                <div>
-                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
-                    <UserAddIcon
-                      className="h-6 w-6 text-blue-600"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <div className="mt-3 sm:mt-5">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg leading-6 font-medium text-gray-900 text-center"
-                    >
-                      Add a contact for {props.vendor.name}
-                    </Dialog.Title>
-                    <div className="space-y-3 mt-4">
-                      {errorMessage ? (
-                        <div className="text-red-500">{errorMessage}</div>
-                      ) : (
-                        <></>
-                      )}
-                      <div className="grid grid-cols-6 gap-3">
-                        <div className="col-span-6 sm:col-span-3">
+                <form onSubmit={handleSubmit(handleContactAddSubmit)}>
+                  <div>
+                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
+                      <UserAddIcon
+                        className="h-6 w-6 text-blue-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg leading-6 font-medium text-gray-900 text-center"
+                      >
+                        Add a contact for {props.vendor.name}
+                      </Dialog.Title>
+                      <div className="space-y-3 mt-4">
+                        {errorMessage ? (
+                          <div className="text-red-500">{errorMessage}</div>
+                        ) : (
+                          <></>
+                        )}
+
+                        <div className="grid grid-cols-6 gap-3">
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="name"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Name
+                            </label>
+                            <div className="mt-1">
+                              <input
+                                id="name"
+                                name="name"
+                                type="text"
+                                {...register("name", { required: true })}
+                                autoComplete="off"
+                                required
+                                className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="title"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Title
+                            </label>
+                            <div className="mt-1">
+                              <input
+                                id="title"
+                                name="title"
+                                type="text"
+                                autoComplete="off"
+                                {...register("title")}
+                                className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="primaryPhone"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Primary phone
+                            </label>
+                            <div className="mt-1">
+                              <input
+                                id="primaryPhone"
+                                name="primaryPhone"
+                                type="text"
+                                {...register("primaryPhone")}
+                                className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="col-span-6 sm:col-span-3">
+                            <label
+                              htmlFor="secondaryPhone"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Secondary phone
+                            </label>
+                            <div className="mt-1">
+                              <input
+                                id="secondaryPhone"
+                                name="secondaryPhone"
+                                type="text"
+                                {...register("secondaryPhone")}
+                                className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                              />
+                            </div>
+                          </div>
+                        </div>
+
+                        <div>
                           <label
-                            htmlFor="name"
+                            htmlFor="email"
                             className="block text-sm font-medium text-gray-700"
                           >
-                            Name
+                            Email address
                           </label>
                           <div className="mt-1">
                             <input
-                              id="name"
-                              name="name"
+                              id="email"
+                              name="email"
                               type="text"
-                              autoComplete="off"
-                              onChange={handleNameChange}
-                              required
-                              className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                              {...register("email")}
+                              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                             />
                           </div>
                         </div>
 
-                        <div className="col-span-6 sm:col-span-3">
+                        <div>
                           <label
-                            htmlFor="title"
+                            htmlFor="description"
                             className="block text-sm font-medium text-gray-700"
                           >
-                            Title
+                            Notes
                           </label>
                           <div className="mt-1">
-                            <input
-                              id="title"
-                              name="title"
-                              type="text"
-                              autoComplete="off"
-                              onChange={handleTitleChange}
-                              required
-                              className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                            <textarea
+                              id="notes"
+                              name="notes"
+                              {...register("notes")}
+                              rows={2}
+                              className="max-w-lg shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
                             />
                           </div>
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-3">
-                          <label
-                            htmlFor="primaryPhone"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            Primary phone
-                          </label>
-                          <div className="mt-1">
-                            <input
-                              id="primaryPhone"
-                              name="primaryPhone"
-                              type="text"
-                              onChange={handlePrimaryPhoneChange}
-                              className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            />
-                          </div>
-                        </div>
-
-                        <div className="col-span-6 sm:col-span-3">
-                          <label
-                            htmlFor="secondaryPhone"
-                            className="block text-sm font-medium text-gray-700"
-                          >
-                            Secondary phone
-                          </label>
-                          <div className="mt-1">
-                            <input
-                              id="secondaryPhone"
-                              name="secondaryPhone"
-                              type="text"
-                              onChange={handleSecondaryPhoneChange}
-                              className="appearance-none inline w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            />
-                          </div>
-                        </div>
-                      </div>
-
-                      <div>
-                        <label
-                          htmlFor="email"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email address
-                        </label>
-                        <div className="mt-1">
-                          <input
-                            id="email"
-                            name="email"
-                            type="text"
-                            onChange={handleEmailChange}
-                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                          />
-                        </div>
-                      </div>
-
-                      <div>
-                        <label
-                          htmlFor="description"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Notes
-                        </label>
-                        <div className="mt-1">
-                          <textarea
-                            id="notes"
-                            name="notes"
-                            rows={2}
-                            className="max-w-lg shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
-                            onChange={handleNotesChange}
-                          />
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-                  <button
-                    type="button"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:col-start-2 sm:text-sm"
-                    onClick={handleContactAddSubmit}
-                  >
-                    Add
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:col-start-1 sm:text-sm"
-                    onClick={props.toggleContactAddEditModal}
-                  >
-                    Cancel
-                  </button>
-                </div>
+                  <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+                    <button
+                      type="submit"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:col-start-2 sm:text-sm"
+                    >
+                      Add
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                      onClick={props.toggleContactAddEditModal}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
               </div>
             </Transition.Child>
           </div>
