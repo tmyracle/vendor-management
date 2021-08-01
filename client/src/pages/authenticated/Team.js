a5b49f69c8c0954a6a9dc4bfa38d6c0f9ab8c4d8
Tyler Myracle <tyler.myracle@gmail.com>
2021-07-31 22:34:36 -0700

    styling

--- client/src/pages/authenticated/Team.js
+++ client/src/pages/authenticated/Team.js
@@ -43,7 +43,7 @@ const Team = (props) => {
         setCompany(res.data.company);
       }
     } catch (error) {
-      toast.error("Error fetching team members")
+      toast.error("Error fetching team members");
     }
   }, []);
 
@@ -83,18 +83,20 @@ const Team = (props) => {
 
   return (
     <div className="mt-8 p-8 max-w-7xl mx-auto">
-      <h1 className="mb-4 text-3xl font-extrabold text-gray-900">
-        {company.name}
-      </h1>
-      <div className="flex justify-end mb-4">
-        <button
-          type="button"
-          onClick={toggleInviteModal}
-          className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-        >
-          <MailIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-          Invite teammate
-        </button>
+      <div className="flex justify-between">
+        <div className="mb-4 text-3xl font-extrabold text-gray-900 inline">
+          {company.name}
+        </div>
+        <div className="flex inline justify-end mb-4">
+          <button
+            type="button"
+            onClick={toggleInviteModal}
+            className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          >
+            <MailIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            Invite teammate
+          </button>
+        </div>
       </div>
       {!company.users ? null : (
         <StripedTable
